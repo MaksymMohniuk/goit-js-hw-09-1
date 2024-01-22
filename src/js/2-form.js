34ee240c28939654792580242cc4ad79713421d9
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-01-23 00:03:01 +0200

    finish-task-9-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -8,12 +8,30 @@ form.addEventListener('input', e => {
         message: form.elements.message.value.trim(),
     }
 localStorage.setItem(localStorageKey, JSON.stringify(data));
-})
+});
+
+function restoreData() {
+    let savedData = localStorage.getItem(localStorageKey);
+    if (savedData) {
+        let {email, message} = JSON.parse(savedData);
+        form.elements.email.value = email || '';
+        form.elements.message.value = message || '';
+    }
+}
+
+restoreData();
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     let savedData = localStorage.getItem(localStorageKey);
-    console.log(JSON.parse(savedData));
-    localStorage.removeItem(localStorageKey);
-    form.reset();
-})
+    if (savedData) {
+        let {email, message} = JSON.parse(savedData);
+        if (email && message) {
+            console.log({email, message});
+            localStorage.removeItem(localStorageKey);
+            form.reset();
+        } else {
+            alert('Please, fill all the fields!');
+        }
+    }
+});
